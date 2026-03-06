bc09bf5e5282aac75e4fba86f2a6c115927edc68
takkytakky16-glitch <takkytakky16@gmail.com>
2026-03-06 13:56:49 +0900

    EnemyにHPバー実装

--- Assets/Script/Enemy/LookStateChange.cs
+++ Assets/Script/Enemy/LookStateChange.cs
@@ -5,21 +5,35 @@ public class LookStateChange : NetworkBehaviour
 {
     [SerializeField] MeshRenderer meshRenderer;
     [SerializeField] PlayerPropaty.PlayerJob lookableJob;
+    [SerializeField] Canvas hpCanvas; // ←HPバーのCanvasをここにアサイン
+
     public override void OnNetworkSpawn()
     {
         PlayerManager playerManager = ManagerLocator.Instance.PlayerManager;
         playerManager.OnOwnerJobChanged += OnJobChangedHandle;
-        if (playerManager.OwnerPlayer != null) 
+
+        if (playerManager.OwnerPlayer != null)
         {
             OnJobChangedHandle(playerManager.OwnerPlayer.propaty.Job);
         }
     }
+
     public override void OnNetworkDespawn()
     {
         ManagerLocator.Instance.PlayerManager.OnOwnerJobChanged -= OnJobChangedHandle;
     }
+
     private void OnJobChangedHandle(PlayerPropaty.PlayerJob job)
-    {   
-        meshRenderer.enabled = (job & lookableJob) != 0;
+    {
+        bool isVisible = (job & lookableJob) != 0;
+
+        // 敵の表示
+        meshRenderer.enabled = isVisible;
+
+        // HPバーの表示も連動
+        if (hpCanvas != null)
+        {
+            hpCanvas.enabled = isVisible;
+        }
     }
 }
