ea9b09850fe3132c36e4a689ac0d207c55e005bd
syacapachi <mountainside200@gmail.com>
2026-05-03 17:05:26 +0900

    難易度ごとにHPを変えられるようにした

---
+++ Assets/Scripts/SO/DifficultySetting.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "DifficultySetting", menuName = "Game/DifficultySetting")]
+public class DifficultySetting : ScriptableObject
+{
+    [SerializeField] Difficulty difficulty;
+    [SerializeField] int playerHP;
+    [SerializeField] PhaseSO[] phaseSO;
+
+    public Difficulty Difficulty => difficulty;
+    public int PlayerHP => playerHP;
+    public PhaseSO[] Phases => phaseSO;
+
+}
+public enum Difficulty
+{
+    Easy,
+    Normal,
+    Hard,
+    Debug
+}
